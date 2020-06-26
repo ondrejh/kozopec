1e73cbe63fe5b5af3c94c06173773daef9c9b05f
ondrejh <ondrejh.ck@email.cz>
2020-06-26 10:32:08 +0200

    forwar, backward, release button and some docu

--- main.c
+++ main.c
@@ -1,26 +1,24 @@
 //******************************************************************************
-// Pomodoro timer for MSP430 launchpad
+// Simple stepper driver with MSP430 mcu
 //
 //
 // author:          Ondrej Hejda
-// date (started):  28.2.2013
+// date (started):  26.6.2020
 //
-// hardware: MSP430G2553 (launchpad)
+// hardware: MSP430G2553 / MSP430G2452
 //
 //                MSP4302553
 //             -----------------
 //         /|\|                 |
-//          | |             XTAL|<---> 32.768kHz quartz (realtime clock)
-//          --|RST              |
-//            |             P1.3|<---- BUTTON
+//          | |             P1.2|<---- BACKWARD
+//          --|RST          P1.3|<---- FORWARD
+//            |             P1.4|<---- RELEASE
 //            |                 |
-//            |             P2.1|----> LED1
-//            |             P2.0|----> LED2
-//            |             P1.5|----> LED3
-//            |             P1.4|----> LED4
-//            |             P1.3|----> LED5
-//            |             P2.5|----> LED6
-//            |             P2.4|----> LED7
+//            |             P1.5|<---- SPEED (analog 0 .. 3.3V)
+//            |                 |
+//            |             P1.0|----> ENABLE
+//            |             P1.1|----> STEP
+//            |             P1.6|----> DIRRECTION
 //            |                 |
 //
 //******************************************************************************
@@ -30,7 +28,7 @@
 //#include <msp430g2452.h>
 #include <stdint.h>
 
-// onboard leds
+// on-board leds
 #define LED_INIT() do{P1DIR|=0x41;P1OUT&=~0x41;}while(0)
 #define LED_RED_ON() do{P1OUT|=0x01;}while(0)
 #define LED_RED_OFF() do{P1OUT&=~0x01;}while(0)
@@ -39,16 +37,27 @@
 #define LED_GREEN_OFF() do{P1OUT&=~0x40;}while(0)
 #define LED_GREEN_SWAP() do{P1OUT^=0x40;}while(0)
 
-// onboard button
+// on-board button
 #define BUTTON_INIT() do{P1DIR&=~BIT3;P1REN|=BIT3;}while(0)
 #define BUTTON ((P1IN&BIT3)==0)
 
-// motor outputs
+// inputs (button with pull-up, active low)
+#define INPUT_INIT() do{P1DIR|=0x1C;P1REN|=0x1C;}while(0)
+#define FORWARD ((P1IN&BIT3)==0)
+#define BACKWARD ((P1IN&BIT2)==0)
+#define RELEASE ((P1IN&BIT4)==0)
+
+// motor outputs (opto-coupled outputs, active low)
 #define MOTOR_INIT() do{P1DIR|0x01;P1OUT&=~0x01;P2DIR|=0x06;P2OUT&=~06;}while(0)
-//#define MOTOR_DIR(x) do{(x)?(P2OUT|=0x02):(P2OUT&=~0x02)}while(0)
+#define MOTOR_DIR(x) do{(x)?(P2OUT|=0x02):(P2OUT&=~0x02);}while(0)
+#define FORW 0
+#define BACK 1
+#define DIR_FORW ((P2OUT&0x02)==0)
+#define DIR_BACK ((P2OUT&0x02)!=0)
 #define MOTOR_STEP() do{P2OUT^=0x04;}while(0)
 #define MOTOR_ENABLE() do{P1OUT|=0x01;}while(0)
 #define MOTOR_DISABLE() do{P1OUT&=~0x01;}while(0)
+#define MOTOR_ENABLED (P1OUT&0x01)
 
 #define STEP_DIV 500 // 2000Hz / 1MHz
 #define STEP_OVF 1000
@@ -69,6 +78,9 @@ void board_init(void)
 
 	// motor
 	MOTOR_INIT();
+
+	// inputs
+	INPUT_INIT();
 
 	// free running timer
 	TACCR0 = 0xFFFF;
@@ -111,29 +123,38 @@ int main(void)
         static int seqv = 0;
         switch (seqv) {
         case 0: // wait button
-            if (BUTTON) {
+            if (FORWARD) {
+                MOTOR_DIR(0);
                 MOTOR_ENABLE();
                 mv = 0;
                 ma = STEP_ACC;
                 seqv++;
             }
+            else if (BACKWARD) {
+                MOTOR_DIR(1);
+                MOTOR_ENABLE();
+                mv = 0;
+                ma = STEP_ACC;
+                seqv++;
+            }
+            else if (RELEASE && MOTOR_ENABLED)
+                MOTOR_DISABLE();
             break;
         case 1: // acceleration (wait button release)
             if (mv >= STEP_OVF)
                 mv = STEP_OVF-1;
-            if (!BUTTON) {
+            if ((DIR_FORW && (!FORWARD)) || (DIR_BACK && (!BACKWARD))) {
                 ma = -STEP_ACC;
                 seqv++;
             }
             break;
         case 2: // deceleration (wait button)
-            if (BUTTON) {
+            if ((DIR_FORW && FORWARD) || (DIR_BACK && (!BACKWARD))) {
                 ma = STEP_ACC;
                 seqv--;
             }
-            else if (mv < 0) {
+            else if (mv <= 0) {
                 mv = 0;
-                MOTOR_DISABLE();
                 seqv = 0;
             }
             break;
